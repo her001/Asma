6fa5d1af57bff9f32efae1661ecb9aa7880937fc
Andrew Conrad <aconrad103@gmail.com>
2017-10-27 15:30:56 -0500

    Add meson_post_install.py
    This updates the icon cache and desktop file database if it is not being
    built for a package.

---
+++ meson_post_install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+prefix = os.environ.get('MESON_INSTALL_PREFIX','/usr/local')
+datadir = os.path.join(prefix, 'share')
+
+if 'DESTDIR' not in os.environ:
+	print('Updating icon cache...')
+	subprocess.call(['gtk-update-icon-cache', '-qtf',
+		os.path.join(datadir, 'icons', 'hicolor')])
+
+	print('Updating desktop database...')
+	subprocess.call(['update-desktop-database', '-q',
+		os.path.join(datadir, 'applications')])
