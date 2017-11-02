d6038bc211f9b6b393b240faa29cc0a0923d421b
Andrew Conrad <aconrad103@gmail.com>
2017-11-02 13:21:01 -0500

    Add GActions for each menu entry
    We now have a functional about dialog and quit button, as well as a
    non-functional preferences window.

--- src/asma.c
+++ src/asma.c
@@ -6,12 +6,80 @@
 #include "errors.h"
 #include "settings.h"
 
+static GActionEntry app_entries[];
+
+
+static void about_activated();
+static void preferences_activated();
+static void quit_activated();
+
+static GActionEntry app_entries[3] = {
+	{"about", about_activated, NULL, NULL, NULL},
+	{"preferences", preferences_activated, NULL, NULL, NULL},
+	{"quit", quit_activated, NULL, NULL, NULL},
+};
+
+static void about_activated(GSimpleAction *action,
+			    GVariant *parameter,
+			    gpointer user_data)
+{
+	GtkBuilder *builder;
+	GtkWindow *dialog;
+	GtkApplication *app = user_data;
+
+	builder = gtk_builder_new_from_resource(
+		"/io/github/busquetsaguilopau/Asma/asma.glade");
+	dialog = GTK_WINDOW (gtk_builder_get_object(builder, "about_dialog"));
+	gtk_window_set_application(dialog, app);
+
+	gtk_window_present(dialog);
+}
+
+static void preferences_activated(GSimpleAction *action,
+				  GVariant *parameter,
+				  gpointer user_data)
+{
+	GtkBuilder *builder;
+	GtkWindow *prefs;
+	GtkApplication *app = user_data;
+
+	builder = gtk_builder_new_from_resource(
+		"/io/github/busquetsaguilopau/Asma/asma.glade");
+	prefs = GTK_WINDOW (gtk_builder_get_object(builder, "prefs_window"));
+	gtk_window_set_application(prefs, app);
+
+	gtk_window_present(prefs);
+}
+
+static void quit_activated(GSimpleAction *action,
+			   GVariant *parameter,
+			   gpointer user_data)
+{
+	GtkApplication *app = user_data;
+	GtkWidget *win;
+	GList *list, *next;
+
+	list = gtk_application_get_windows(app);
+	while (list) {
+		win = list->data;
+		next = list->next;
+
+		gtk_widget_destroy(win);
+
+		list = next;
+	}
+}
+
 static void startup(GtkApplication *app)
 {
 	GtkBuilder *builder;
 	GMenuModel *menu;
 
 	g_assert(GTK_IS_APPLICATION (app));
+
+	g_action_map_add_action_entries(G_ACTION_MAP (app),
+					app_entries, G_N_ELEMENTS (app_entries),
+					app);
 
 	builder = gtk_builder_new_from_resource(
 		"/io/github/busquetsaguilopau/Asma/gmenu.ui");
