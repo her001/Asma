54847c1e1493c29f1b5c59f25d9857dad2edf482
Andrew Conrad <aconrad103@gmail.com>
2017-11-06 18:30:31 -0600

    Make mod list use toggleable buttons
    The selection behavior of GtkListBox was undesirable. Now clicking an
    item toggles the mod (with an icon to indicate it).

--- src/mods.c
+++ src/mods.c
@@ -3,13 +3,26 @@
 #include "mods.h"
 #include "asma.h"
 
+static void toggle_image(GtkButton *button,
+                         gpointer   user_data)
+{
+	if (gtk_button_get_always_show_image(button))
+		gtk_button_set_always_show_image(button, FALSE);
+	else
+		gtk_button_set_always_show_image(button, TRUE);
+}
+
 static GtkWidget* create_list_item(gpointer item,
                                    gpointer user_data)
 {
-	gtk_widget_set_margin_start(GTK_WIDGET (item), 12);
-	gtk_widget_set_margin_end(GTK_WIDGET (item), 12);
-	gtk_widget_set_margin_top(GTK_WIDGET (item), 6);
-	gtk_widget_set_margin_bottom(GTK_WIDGET (item), 6);
+	GtkWidget *check;
+
+	check = gtk_image_new_from_icon_name("object-select-symbolic", GTK_ICON_SIZE_BUTTON);
+	gtk_widget_set_margin_start(check, 6);
+	gtk_button_set_image(GTK_BUTTON (item), check);
+	gtk_button_set_image_position(GTK_BUTTON(item), GTK_POS_RIGHT);
+	gtk_button_set_relief(item, GTK_RELIEF_NONE);
+	g_signal_connect(item, "toggled", (gpointer) &toggle_image, NULL);
 	return GTK_WIDGET (item);
 }
 
@@ -17,8 +30,8 @@ static gint sort_alpha(const void *a,
 		       const void *b,
 		       void *user_data)
 {
-	const gchar *x = gtk_label_get_text(GTK_LABEL (a));
-	const gchar *y = gtk_label_get_text(GTK_LABEL (b));
+	const gchar *x = gtk_button_get_label(GTK_BUTTON (a));
+	const gchar *y = gtk_button_get_label(GTK_BUTTON (b));
 	return g_strcmp0((gchar *) x, (gchar *) y);
 }
 
@@ -28,7 +41,7 @@ static GListStore* get_local_mods()
 	GListStore *mods;
 	gchar *entry = "";
 
-	mods = g_list_store_new(g_type_from_name("GtkLabel"));
+	mods = g_list_store_new(g_type_from_name("GtkToggleButton"));
 
 	dir = g_dir_open(g_file_get_path(arma3_root), 0, NULL);
 	if (dir == NULL) {
@@ -41,9 +54,9 @@ static GListStore* get_local_mods()
 			const gchar *abs_path;
 			abs_path = g_file_get_path(g_file_get_child(arma3_root, entry));
 			if (g_file_test(abs_path, G_FILE_TEST_IS_DIR)) {
-				GtkWidget *label;
-				label = gtk_label_new(entry);
-				g_list_store_append(mods, (gpointer) label);
+				GtkToggleButton *button;
+				button = GTK_TOGGLE_BUTTON (gtk_toggle_button_new_with_label(entry));
+				g_list_store_append(mods, (gpointer) button);
 			}
 		}
 		entry = g_strdup(g_dir_read_name(dir));
